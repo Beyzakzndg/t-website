8432ccabbf084ee23eba96a0d0839a689d864877
beyzaozp <beyzamunireozpinar@gmail.com>
2023-10-31 14:39:44 +0300

    align title

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -68,89 +68,91 @@ function Category() {
                         <Sidebar />
                     </Stack>
                     <Stack p={2} width={"100%"} >
-                        <Stack bgcolor="rgba(255, 255, 255, 0.5)" border="1px solid #aaa" borderRadius={2} justifyContent={{ xs: "center", xl: "flex-start" }} p={3} direction="row" gap={3} sx={{ flexWrap: "wrap" }} >
-                            <Stack sx={{ boxShadow: "4px 4px 25px 0px #222121", background: "linear-gradient(94deg, rgba(223, 107, 0, 0.5) 1.33%, rgba(255, 230, 0, 0.5) 52.99%)" }} p="5% 25%" borderRadius={4}>
-                                <Typography variant='h5' fontWeight={500}>
+                        <Stack bgcolor="rgba(255, 255, 255, 0.5)" border="1px solid #aaa" borderRadius={2} >
+                            <Stack width={{xs:"70%",xl:"50%"}} alignSelf="center" textAlign="center" mt={3} sx={{ boxShadow: "4px 4px 25px 0px #222121", background: "linear-gradient(94deg, rgba(223, 107, 0, 0.5) 1.33%, rgba(255, 230, 0, 0.5) 52.99%)" }} p="2% 10%" borderRadius={4}>
+                                <Typography fontSize="24px" fontWeight={500}>
                                     {title}
                                 </Typography>
                             </Stack>
-                            {router.query.category === "forklift" && data.map((item) => (
-                                <Stack alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
-                                    <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
-                                    <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
-                                </Stack>
-                            ))}
+                            <Stack justifyContent={{ xs: "center", xl: "flex-start" }} p={3} direction="row" gap={3} sx={{ flexWrap: "wrap" }}>
+                                {router.query.category === "forklift" && data.map((item) => (
+                                    <Stack alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
+                                        <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
+                                        <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
+                                    </Stack>
+                                ))}
 
-                            {router.query.category === "elektrikli-forklift" && data.map((item) => (
-                                <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
-                                    <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
-                                    <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
-                                </Stack>
-                            ))}
+                                {router.query.category === "elektrikli-forklift" && data.map((item) => (
+                                    <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
+                                        <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
+                                        <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
+                                    </Stack>
+                                ))}
 
-                            {router.query.category === "akulu-istif" && data.map((item) => (
-                                <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
-                                    <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
-                                    <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
-                                </Stack>
-                            ))}
+                                {router.query.category === "akulu-istif" && data.map((item) => (
+                                    <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
+                                        <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
+                                        <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
+                                    </Stack>
+                                ))}
 
-                            {router.query.category === "manuel-istif" && data.map((item) => (
-                                <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
-                                    <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
-                                    <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
-                                </Stack>
-                            ))}
+                                {router.query.category === "manuel-istif" && data.map((item) => (
+                                    <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
+                                        <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
+                                        <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
+                                    </Stack>
+                                ))}
 
-                            {router.query.category === "manuel-transpalet" && dataTranspalet.map((item) => (
-                                <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
-                                    <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
-                                    <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
-                                </Stack>
-                            ))}
+                                {router.query.category === "manuel-transpalet" && dataTranspalet.map((item) => (
+                                    <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
+                                        <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
+                                        <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
+                                    </Stack>
+                                ))}
 
-                            {router.query.category === "akulu-transpalet" && dataTranspalet.map((item) => (
-                                <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
-                                    <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
-                                    <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
-                                </Stack>
-                            ))}
+                                {router.query.category === "akulu-transpalet" && dataTranspalet.map((item) => (
+                                    <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
+                                        <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
+                                        <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
+                                    </Stack>
+                                ))}
 
-                            {router.query.category === "terazili-transpalet" && dataTranspalet.map((item) => (
-                                <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
-                                    <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
-                                    <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
-                                </Stack>
-                            ))}
-                            {router.query.category === "havali-lastikler" && dataTranspalet.map((item) => (
-                                <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
-                                    <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
-                                    <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
-                                </Stack>
-                            ))}
-                            {router.query.category === "dolgu-lastikler" && dataTranspalet.map((item) => (
-                                <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
-                                    <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
-                                    <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
-                                </Stack>
-                            ))}
-                            {router.query.category === "beyaz-dolgu-lastikler" && dataTranspalet.map((item) => (
-                                <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
-                                    <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
-                                    <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
-                                </Stack>
-                            ))}
-                            {router.query.category === "manlift" && dataTranspalet.map((item) => (
-                                <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
-                                    <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
-                                    <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
-                                </Stack>
-                            ))}
+                                {router.query.category === "terazili-transpalet" && dataTranspalet.map((item) => (
+                                    <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
+                                        <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
+                                        <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
+                                    </Stack>
+                                ))}
+                                {router.query.category === "havali-lastikler" && dataTranspalet.map((item) => (
+                                    <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
+                                        <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
+                                        <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
+                                    </Stack>
+                                ))}
+                                {router.query.category === "dolgu-lastikler" && dataTranspalet.map((item) => (
+                                    <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
+                                        <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
+                                        <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
+                                    </Stack>
+                                ))}
+                                {router.query.category === "beyaz-dolgu-lastikler" && dataTranspalet.map((item) => (
+                                    <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
+                                        <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
+                                        <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
+                                    </Stack>
+                                ))}
+                                {router.query.category === "manlift" && dataTranspalet.map((item) => (
+                                    <Stack border="1px solid #aaaa" alignItems="center" sx={{ borderRadius: 8, margin: 0 }} key={item.id} bgcolor="#423F3F">
+                                        <Image style={{ borderTopRightRadius: 8, borderTopLeftRadius: 8 }} src={item.img} width={361} height={400} alt="test" />
+                                        <Typography sx={{ height: "55px", padding: "15px", color: "#ffff" }}>{item.name}</Typography>
+                                    </Stack>
+                                ))}
+                            </Stack>
                         </Stack>
                     </Stack>
                 </Stack>
 
-            </Layout>
+            </Layout >
         </>
     );
 }
